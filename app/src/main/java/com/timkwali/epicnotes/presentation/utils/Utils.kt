d67b2a5792a2375a0410632c98e3ebcb32b9676d
timkwali <timkwalit@accessbankplc.com>
2021-12-16 13:47:09 +0100

    set up time picker

--- app/src/main/java/com/timkwali/epicnotes/presentation/utils/Utils.kt
+++ app/src/main/java/com/timkwali/epicnotes/presentation/utils/Utils.kt
@@ -16,38 +16,58 @@ import java.util.*
 
 object Utils {
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun showDatePicker(fragmentManager: FragmentManager, tag: String, textView: TextView) {
         val picker = MaterialDatePicker.Builder.datePicker()
             .setTitleText("Select date")
             .build()
         picker.addOnPositiveButtonClickListener {
-            val selectedDate = picker.selection?.toInt()
+            val selectedDate = it
             val c = Calendar.getInstance()
             val today = SimpleDateFormat("dd-MM-yyyy")
-                .parse("${c.get(Calendar.DAY_OF_MONTH)}-${c.get(Calendar.MONTH)}-${c.get(Calendar.YEAR)}")
+                .parse("${c.get(Calendar.DAY_OF_MONTH)}-${c.get(Calendar.MONTH + 1)}-${c.get(Calendar.YEAR)}")
                 .time
 
-            if (selectedDate != null) {
-                if(selectedDate < today) {
-                    textView.findFragment<Fragment>().showSnackBar("Please select a valid date!")
-                } else {
-                    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-                    calendar.time = Date(it)
-                    textView.text = "${calendar.get(Calendar.DAY_OF_MONTH)}-" +
-                            "${calendar.get(Calendar.MONTH) + 1}-${calendar.get(Calendar.YEAR)}"
-                }
-            }
+//            if(selectedDate < today) {
+//                textView.findFragment<Fragment>().showSnackBar("Please select a valid date!")
+//            } else {
+                val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                calendar.time = Date(selectedDate)
+                textView.text = "${calendar.get(Calendar.DAY_OF_MONTH)}-" +
+                        "${calendar.get(Calendar.MONTH + 1)}-${calendar.get(Calendar.YEAR)}"
+//            }
         }
         picker.show(fragmentManager, tag)
     }
 
     fun showTimePicker(fragmentManager: FragmentManager, tag: String, textView: TextView) {
+
         val picker = MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setTimeFormat(TimeFormat.CLOCK_24H)
                 .setTitleText("Select Appointment time")
                 .build()
-//        picker.addOnPositiveButtonClickListener { textView.text = picker.get }
+        picker.addOnPositiveButtonClickListener {
+            var hour = ""
+            var meridian = ""
+            var minute = ""
+            when {
+                picker.hour > 12 -> {
+                    meridian = "P.M."
+                    hour = "${picker.hour - 12}"
+                }
+                picker.hour == 12 -> {
+                    hour = "${picker.hour}"
+                    meridian = "P.M."
+                }
+                else ->  {
+                    meridian = "A.M."
+                    hour = "${picker.hour}"
+                    if(picker.hour < 10) hour = "0$hour"
+                }
+            }
+            minute = if(picker.minute < 10) "0${picker.minute}" else "${picker.minute}"
+            textView.text = "$hour:$minute $meridian"
+
+        }
         picker.show(fragmentManager, tag)
     }
 
